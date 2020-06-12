326d0597e0631a8644003c794edf07fc6288488a
Awais Tariq <awais-join>
2020-06-12 12:30:45 -0500

    removed unused imports

--- src/main/java/com/fetchrewards/test/EmailChecker/service/EmailCheckerService.java
+++ src/main/java/com/fetchrewards/test/EmailChecker/service/EmailCheckerService.java
@@ -5,10 +5,6 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Service
 public class EmailCheckerService {
